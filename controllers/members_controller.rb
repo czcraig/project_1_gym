f72dcbd8fbc869ca7db288b745966c72d14f0351
Chris Craig <ctc2@live.co.uk>
2018-06-19 10:21:09 +0100

    member page has show and create page

--- controllers/members_controller.rb
+++ controllers/members_controller.rb
@@ -9,3 +9,15 @@ get '/members' do
   @members = Member.all()
   erb(:'members/index')
 end
+
+# new
+get '/members/new' do
+  @classes = GymClass.all()
+  erb( :'members/create' )
+end
+
+# SHOW
+get '/members/:id' do
+  @member = Member.find(params[:id].to_i())
+  erb( :'members/show' )
+end
